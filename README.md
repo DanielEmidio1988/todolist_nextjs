1ba0ba39dfaede514d79b638042804b510292a82
DanielEmidio1988 <daniel.emidio.silva@gmail.com>
2023-06-06 14:15:29 -0300

    Finalização projeto

---
+++ README.md
@@ -0,0 +1,50 @@
+# Todo-list: NextJs
+
+## 📖 Introdução 
+
+Este é um projeto de estudo, para auxiliar no desenvolvimento de aplicações web utilizando NextJs. Trata-se de uma To-do List, onde o usuário pode inserir novas atividades.
+
+## 🔗Link de Acesso
+- Deploy Vercel: Em Breve
+
+## 👥Equipe
+| [<img src="https://avatars.githubusercontent.com/u/111311678?v=4" width=115><br><sub>Daniel Emidio</sub>](https://github.com/DanielEmidio1988) |
+| :---: |
+
+## 🧭Status do Projeto
+- ⏳Concluido
+
+## 📄Concepção do Projeto
+
+### Instalando
+```bash
+# Instalando dependências
+npm install
+
+# executando o projeto
+npm start
+```
+
+### Funcionalidades
+```bash
+ - Inserir novas atividades, ao clicar pelo botão ou inserir Enter.
+```
+
+## 💡Programas utilizados:
+- VSCode
+
+## 💻Tecnologias 
+
+![CSS](https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white)
+![HTML](https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white)
+![Javascript](https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E)
+![NextJs](https://img.shields.io/badge/next.js-000000?style=for-the-badge&logo=nextdotjs&logoColor=white)
+![Git](https://img.shields.io/badge/GIT-E44C30?style=for-the-badge&logo=git&logoColor=white)
+
+
+## 📫 Contato
+
+E-mail: emidio.daniel@hotmail.com
+
+[![LinkedIn](https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/danielemidio1988/)
+[![Codewars](https://img.shields.io/badge/Codewars-B1361E?style=for-the-badge&logo=Codewars&logoColor=white)](https://www.codewars.com/users/DanielEmidio1988)
--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,6 @@ import styles from "../styles/Home.module.css"
 export default function Home() {
   const [taskName, setTaskName] = useState('')
   const [tasksData, setTaskData] = useState([{task: "Jogar Video Game", completed: false}, {task: "Limpar a Casa", completed: true}])
-  const [optionsTask, setOptionTask] = useState("")
 
   const insertTask = (taskName)=>{
     const dataTask = {
@@ -13,6 +12,23 @@ export default function Home() {
       completed: false
     }
     setTaskData((prevTasks)=>[...prevTasks, dataTask])
+    setTaskName('')
+  }
+
+  const deleteTask = (taskName)=>{
+    const dataTask = tasksData.filter((task)=> {
+      return task.task !== taskName
+    })
+    setTaskData(dataTask)
+  }
+
+  const updateStatusTask = (taskInfo)=>{
+    const dataTask = tasksData.filter((task)=> {
+      return task.task !== taskInfo.task
+    })
+    const auxTask = {...taskInfo, completed: !taskInfo.completed}
+    dataTask.push(auxTask)
+    setTaskData(dataTask)
   }
 
   return (
@@ -20,48 +36,53 @@ export default function Home() {
 
     <section className={styles.section_input_task}>
       <div>
-        <input value={taskName} onChange={(event)=>setTaskName(event.target.value)} placeholder="Adicione uma tarefa"/>
+        <input 
+        value={taskName} 
+        onChange={(event)=>setTaskName(event.target.value)} 
+        onKeyDown={(event)=> event.key === 'Enter' ? insertTask(taskName, optionsTask) : ''}
+        placeholder="Adicione uma tarefa"/>
       </div>
       <div>
-        <select value={optionsTask} onChange={(event)=>setOptionTask(event.target.value)}>
-          <option value="">Escolha uma opção</option>          
-          <option value="Estudos">Estudos</option>
-          <option value="Trabalho">Trabalho</option>
-          <option value="Lazer">Lazer</option>
-          <option value="Saúde">Saúde</option>
-        </select>
         <button onClick={()=>insertTask(taskName)}>Incluir Tarefa</button>
       </div>
-
     </section>
+
     <section className={styles.section_detail_tasks}>
       <h2>Tarefas em Aberto</h2>
         {tasksData.length > 0 ?
         tasksData.filter((task)=> !task.completed).map((task, index)=>{
           return(
-            <div key={index} className={styles.box_task}>
+            <div key={index} className={styles.box_task} onDoubleClick={()=>updateStatusTask(task)}>
               <div>
                 <h3>{task.task}</h3>
               </div>
               <div>
-                <input type="checkbox"/>
-                <Image src="/images/pencil.svg" width="20" height="20" alt="editar-tarefa"/>
-                <Image src="/images/trash-bin.svg" width="20" height="20" alt="editar-tarefa"/>
-              </div>
-              
+                <Image className={styles.button_task} src="/images/trash-bin.svg" width="20" height="32" alt="excluir-tarefa" onClick={()=>deleteTask(task.task)}/>
+              </div>  
             </div>
           )
         })
         :
         <p>Nenhuma tarefa cadastrada!</p>}
-
-      <div>
-
-      </div>
-
     </section>
-    <section>
-      <h1></h1>
+
+    <section className={styles.section_detail_tasks}>
+      <h2>Tarefas Concluidas</h2>
+          {tasksData.length > 0 ?
+          tasksData.filter((task)=> task.completed).map((task, index)=>{
+            return(
+              <div key={index} className={styles.box_task} onDoubleClick={()=>updateStatusTask(task)}>
+                <div>
+                  <h3 className={styles.text_task_finish}>{task.task}</h3>
+                </div>
+                <div>
+                  <Image className={styles.button_task} src="/images/trash-bin.svg" width="20" height="32" alt="excluir-tarefa" onClick={()=>deleteTask(task.task)}/>
+                </div>  
+              </div>
+            )
+          })
+        :
+        <p>Nenhuma tarefa cadastrada!</p>}
     </section>
     </>
   )
